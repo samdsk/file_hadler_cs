241ca867890cfa9d599d193a361a6953e6b3c236
PhantomAgent47 <sam.ds@live.com>
2020-06-28 18:56:10 +0200

    inizial

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace File_Handler_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source Folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Destination Folder";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(111, 68);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(427, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(544, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(544, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(646, 121);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "File Handler";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 356);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(646, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(427, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.ForeColor = System.Drawing.Color.Lime;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 140);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(646, 210);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(544, 97);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Start!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(670, 391);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "File Handler by Sam Ds ver. 1.01 PreAlpha";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Handler_Form
+{
+    public partial class Form1 : Form
+    {
+
+        string sourcePath;
+        string destinationPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Select the source folder";
+            fbd.RootFolder = Environment.SpecialFolder.MyComputer;
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                sourcePath = fbd.SelectedPath;
+                textBox1.Text = sourcePath;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Select the destination folder";
+            fbd.RootFolder = Environment.SpecialFolder.MyComputer;
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                destinationPath = fbd.SelectedPath;
+                textBox2.Text = destinationPath;
+            }
+        }
+
+        
+
+        private const string File_Name = "FileHandlerLog.txt";
+
+        public void FileHandler() {
+
+            appendOutput("This app let you handle large amount of music files in bulk ordered by Artist's name..");                      
+            //appendOutput("For greater efficiency of the program is recommended to have files");
+            //appendOutput("named like: Artist - song name.mp3");
+            appendOutput("FileHandler_Form version. 1.01 By Sam Ds \n");
+
+            if (Directory.Exists(sourcePath))
+            {
+                appendOutput("Source path found.");
+
+                if (Directory.Exists(destinationPath))
+                {
+                    appendOutput("Destination path found.");
+
+                    appendOutput("Creating Log File at Destination.");
+                    appendOutput("Wait we are making your life easy... \n");
+
+                    string filePath = Path.Combine(destinationPath, File_Name);
+
+                    if (File.Exists(File_Name))
+                    {
+                        appendOutput("Log found, deleting old one.");
+
+                        File.Delete(filePath);
+
+                    }
+                    else
+                    {
+
+                        // Get source dir's files
+                        string[] sourceFiles = Directory.GetFiles(sourcePath);
+
+                        int percentageIndexer = 1;
+                        int percentage = sourceFiles.Length;
+                        foreach (string sourceFile in sourceFiles)
+                        {
+
+                            // Get destination dir's subdirs
+                            string[] destinationSubDirs = Directory.GetDirectories(destinationPath);
+
+
+                            ArrayList subDirList = DestinationSubDirNames(destinationSubDirs);
+                            string fileToHandle = Path.GetFileName(sourceFile);
+                            string fileArtistName = NameSplitter(sourceFile);
+
+                            int folderIndexer = subDirList.IndexOf((object)fileArtistName);
+                           
+                            appendOutput("Currently handling file:");
+                            appendOutput(fileToHandle);
+                            
+
+                            if (folderIndexer != -1)
+                            {
+                                //Get Artist folder's files
+                                string dArtistPath = Path.Combine(destinationPath, fileArtistName);
+                                int subFolderIndexer = Array.IndexOf(destinationSubDirs, dArtistPath);
+                                string[] dArtistDirFiles = Directory.GetFiles(destinationSubDirs[subFolderIndexer]);
+                                string dArtistFileFullPath = Path.Combine(destinationSubDirs[subFolderIndexer], fileToHandle);
+                                int subDirFileChecker = Array.IndexOf(dArtistDirFiles, dArtistFileFullPath);
+
+                                if (subDirFileChecker == -1)
+                                {
+                                    //appendOutput("File: {0} not found in subdir.", fileToHandle);
+                                    CopyingFile(dArtistFileFullPath, sourceFile, fileToHandle);
+                                    appendOutput("COPYING - File not found in subdir.");
+
+                                }
+                                else
+                                {
+                                    appendOutput("SKIPPING - File found in folder.");
+                                }
+
+                            }
+                            else
+                            {
+                                //appendOutput("Folder not found, Artist: {0}.", fileArtistName);
+                                string dirToCreate = Path.Combine(destinationPath, fileArtistName);
+
+                                Directory.CreateDirectory(dirToCreate);
+                                if (Directory.Exists(dirToCreate))
+                                {/*
+                                    appendOutput("Directory created:");
+                                    appendOutput(fileArtistName);
+                                    appendOutput(fileToHandle);
+                                    appendOutput("File Copied");
+                                    appendOutput("\n");
+                                   */
+                                    string newDirFileFullPath = Path.Combine(dirToCreate, fileToHandle);
+                                    CopyingFile(newDirFileFullPath, sourceFile, fileToHandle);
+                                }
+                                else
+                                {
+                                    appendOutput("Error While creating folder:");
+                                    appendOutput(fileArtistName);
+                                }
+
+                            }
+                            //Percentage display
+                            int percentageOfTask = (percentageIndexer * 100) / percentage;
+                            /*
+                            Console.SetCursorPosition(0, 20);
+                            appendOutput("\r\n Completed: {0}%   ", percentageOfTask);
+                            */
+                            percentageIndexer++;
+                        }
+                    }
+                }
+                else
+                {
+                    appendOutput("Destination directory doesn't exists!");
+                }
+
+            }
+            else
+            {
+                appendOutput("Source directory doesn't exists!");
+            }
+
+            Console.ReadLine();
+        } //End of Main funtion (File Handler)
+
+
+
+
+        private void appendOutput(String msg)
+        {
+            richTextBox1.AppendText(msg + "\n");
+        }
+
+        private void appendError(String msg, bool clearPrior)
+        {
+            if (clearPrior)
+            {
+                richTextBox1.Clear();
+            }
+
+            richTextBox1.SelectionColor = Color.Red;
+            richTextBox1.SelectedText = msg + "\r\n";
+        }
+
+
+        // Make an array of destination subdirectories
+        static ArrayList DestinationSubDirNames(string[] dPath)
+        {
+
+            ArrayList subDirList = new ArrayList();
+
+            foreach (string subdir in dPath)
+            {
+
+                string subDirName = Path.GetFileName(subdir);
+                subDirList.Add(subDirName);
+
+            }
+
+            return subDirList;
+        }
+
+        // Split source file name to find out the artist
+        static string NameSplitter(string sourceFile)
+        {
+            string sourceFileName = Path.GetFileName(sourceFile);
+            string sourceFileNameNoExtend = Path.GetFileNameWithoutExtension(sourceFile);
+
+            string[] spliter1 = { "Feat.", "Feat", "feat.", "Feat.", "Ft.", " - ","-"," -","ft."};
+
+            string[] splitedFileNames = sourceFileNameNoExtend.Split(spliter1, StringSplitOptions.None);
+
+            string splitedFile = splitedFileNames[0];
+
+            string[] spliter2 = { " vs ", "vs.", "Vs.", " Vs ", "&", "(", "," };
+            string[] splitedFileArtists = splitedFile.Split(spliter2, StringSplitOptions.None);
+
+            char[] charsToTrim = { ' ', '-', '.' };
+            string fileArtist = splitedFileArtists[0].Trim(charsToTrim);
+
+            return fileArtist;
+        }
+
+        static void CopyingFile(string dArtistFileFullPath, string sourceFile, string fileToHandle)
+        {
+            string dPathToHandle = dArtistFileFullPath;
+            string sPathToHandle = sourceFile;
+
+            try
+            {
+
+                // Copy the file.
+                File.Copy(sPathToHandle, dPathToHandle);
+
+                File.Copy(sPathToHandle, dPathToHandle, true);
+                //appendOutput("Copy operation succeeded: {0}",fileToHandle);
+            }
+
+            catch
+            {
+               // appendOutput("Error while coping: {0}", fileToHandle);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FileHandler();
+        }
+    }
+}
